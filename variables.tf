f3ae10aa15f944a2c76e89964614f67dd0b3be33
santoshswain03 <santoshhuna@gmail.com>
2026-04-07 21:08:02 +0530

    test1_cicd

--- variables.tf
+++ variables.tf
@@ -1,5 +1,21 @@
-variable "organization_name" { type = string }
-variable "account_name"      { type = string }
-variable "user"              { type = string }
-variable "password"          { type = string; sensitive = true }
-variable "role"              { type = string; default = "SYSADMIN" }
+variable "organization_name" {
+  type = string
+}
+
+variable "account_name" {
+  type = string
+}
+
+variable "user" {
+  type = string
+}
+
+variable "password" {
+  type      = string
+  sensitive = true
+}
+
+variable "role" {
+  type    = string
+  default = "SYSADMIN"
+}
